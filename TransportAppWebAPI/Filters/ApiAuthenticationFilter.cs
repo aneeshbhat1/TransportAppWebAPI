af80ee6534053eb71c419d48c664334411dd0d7a
aneeshbhat1 <aneudupi@gmail.com>
2017-11-09 01:40:34 +0530

    Adding missed out files!

---
+++ TransportAppWebAPI/Filters/ApiAuthenticationFilter.cs
@@ -0,0 +1,57 @@
+﻿using BusinessInterfaces;
+using DALResolver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Http.Controllers;
+
+namespace TransportAppWebAPI.Filters
+{
+    /// <summary>
+    /// Custom Authentication Filter Extending basic Authentication
+    /// </summary>
+    public class ApiAuthenticationFilter : GenericAuthenticationFilter
+    {
+        /// <summary>
+        /// Default Authentication Constructor
+        /// </summary>
+        public ApiAuthenticationFilter()
+        {
+        }
+
+        /// <summary>
+        /// AuthenticationFilter constructor with isActive parameter
+        /// </summary>
+        /// <param name="isActive"></param>
+        public ApiAuthenticationFilter(bool isActive)
+            : base(isActive)
+        {
+        }
+
+        /// <summary>
+        /// Protected overriden method for authorizing user
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="actionContext"></param>
+        /// <returns></returns>
+        protected override bool OnAuthorizeUser(string username, string password, HttpActionContext actionContext)
+        {
+            var provider = DALFactory.Instance.UserService as IUserServices;
+            if (provider != null)
+            {
+                var userId = provider.AuthenticateUser(username, password);
+                if (userId > 0)
+                {
+                    var basicAuthenticationIdentity = Thread.CurrentPrincipal.Identity as BasicAuthenticationIdentity;
+                    if (basicAuthenticationIdentity != null)
+                        basicAuthenticationIdentity.UserId = userId;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
